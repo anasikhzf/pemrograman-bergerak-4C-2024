a7bd090135f36b0d0fcc005135b3f6020e83f67d
shafirazhr <shafirazhr28@gmail.com>
2024-03-28 18:12:27 +0700

    220441100101-SHAFIRA ZUKHRUFATUZ ZAHRA-MUHAMMAD IQBAL FIRMANSYAH-MODUL 1

--- modul-1/220441100101-ShafiraZukhrufatuzZahra/Main.kt
+++ modul-1/220441100101-ShafiraZukhrufatuzZahra/Main.kt
@@ -1,11 +1,9 @@
 // IntelliJ API Decompiler stub source generated from a class file
 // Implementation of methods is not available
-class Produk(val nama: String, val harga: Double, val stok: Int)
+class Produk(val nama: String, val harga: Double, val stok: Int){
 
-fun main() {
-    baru()
 }
-    fun baru(){
+fun main() {
     while (true) {
         println("=========================")
         println("|      Data Produk       |")
@@ -14,40 +12,40 @@ fun main() {
         println("|1. Bahan Masak          |")
         println("|2. Bahan Roti           |")
         print("Masukan pilihan 1-2: ")
-        var pilihan = readLine()?.toIntOrNull()
+        val pilihan = readLine()?.toIntOrNull()
 
         if (pilihan == 1) {
             println("========================")
             println("|Masukan Produk Bahan Masak|")
             println("========================")
             print("|  Masukan Nama Produk  |")
-            var nama = readLine() ?: ""
+            val nama = readLine() ?: ""
             print("|  Masukan Harga Produk  |")
-            var harga = readLine()?.toDoubleOrNull() ?: 0.0
+            val harga = readLine()?.toDoubleOrNull() ?: 0.0
             print("|  Masukan Stok Produk   |")
-            var stok = readLine()?.toIntOrNull() ?: 0
+            val stok = readLine()?.toIntOrNull() ?: 0
             println("========================")
 
-            var produk = Produk(nama, harga, stok)
+            val produk = Produk(nama, harga, stok)
         }else if (pilihan == 2) {
             println("========================")
             println("|Masukan Produk Bahan Roti|")
             println("========================")
             print("|  Masukan Nama Produk  |")
-            var nama = readLine() ?: ""
-            print("|    Masukan Harga Produk    |")
-            var harga = readLine()?.toDoubleOrNull() ?: 0.0
+            val nama = readLine() ?: ""
+            print("|  Masukan Harga Produk  |")
+            val harga = readLine()?.toDoubleOrNull() ?: 0.0
             print("|  Masukan Stok Produk   |")
-            var stok = readLine()?.toIntOrNull() ?: 0
+            val stok = readLine()?.toIntOrNull() ?: 0
             println("========================")
 
-            var produk = Produk(nama, harga, stok)
+            val produk = Produk(nama, harga, stok)
         } else {
             println("Pilihan tidak tersedia")
         }
 
         print("Masukkan Produk Lagi? (Y/T): ")
-        var ulang = readLine()
+        val ulang = readLine()
         if (ulang?.equals("Y", ignoreCase = true) == true) {
         }else {
             println("==========================================")
