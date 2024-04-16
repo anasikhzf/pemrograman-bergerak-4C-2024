b501fbd0fd4785b9ad27bf92105771b9d40837f9
Arumrahma20 <arumrahmadhani6693@gmail>
2024-04-16 08:43:08 +0700

    220441100074-ArumRahmadhani-IqbalFirmansyah-Modul2

---
+++ modul-2/220441100074_ARUM RAHMADHANI/Praktikum2/app/src/main/java/com/example/praktikum2/Halaman3.kt
@@ -0,0 +1,12 @@
+package com.example.praktikum2
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Halaman3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hal3)
+    }
+}
