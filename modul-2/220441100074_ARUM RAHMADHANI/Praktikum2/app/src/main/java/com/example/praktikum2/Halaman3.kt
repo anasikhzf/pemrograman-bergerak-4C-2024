78581b02771c15329387bb126ad6427568730b4e
Arumrahma20 <arumrahmadhani6693@gmail>
2024-05-01 16:56:30 +0700

    220441100074-ARUM RAHMADHANI-M IQBAL FIRMANSYAH-MODUL 2

--- modul-2/220441100074_ARUM RAHMADHANI/Praktikum2/app/src/main/java/com/example/praktikum2/Halaman3.kt
+++ modul-2/220441100074_ARUM RAHMADHANI/Praktikum2/app/src/main/java/com/example/praktikum2/Halaman3.kt
@@ -1,12 +1,20 @@
 package com.example.praktikum2
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class Halaman3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hal3)
+
+        val imageView = findViewById<ImageView>(R.id.utm1)
+        imageView.setOnClickListener {
+            val intent = Intent(this, Halaman1::class.java)
+            startActivity(intent)
+        }
     }
 }
