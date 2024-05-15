d0f65f6bf949ce2e97a4a5097a06cce586ec16f6
dellaayuu <dellaayu901@gmail.com>
2024-05-15 20:56:59 +0700

    220441100108-DELLA AYU VERNANDAH-MUHAMMAD IQBAL FIRMANSYAH-MODUL 3

---
+++ modul-3/220441100108-DellaAyuVernandah/app/src/main/java/com/example/modultiga/data/DestinasiList.kt
@@ -0,0 +1,165 @@
+package com.example.modultiga.data
+
+import com.example.modultiga.R
+
+object DestinasiList {
+    val LokasiDum = arrayListOf(
+        LocationData(
+            "National Park Yosemite",
+            "Desa Wisata Bahari Nglebeng di Kabupaten Trenggalek mempunyai produk wisata Pantai Kuyon. Pantai Kuyon memiliki keindahan pantai yang berbatasan dengan sawah yang menambah kecantikan pantai. Pantai Kuyon dapat dijadikan lokasi liburan di akhir pekan bersama teman-teman untuk refreshing.\n",
+            "Trenggalek",
+            R.drawable.img_4
+        ),
+        LocationData(
+            "Whitehaven Beach",
+            "Pantai Kebo Trenggalek memiliki spot yang cukup istimewa berupa rerumputan hijau luas dengan aksen aliran air bagaikan sungai dan beberapa ekor kerbau. Latar belakang pantai berupa bukit-bukit hijau yang mampu menambah alami pemandangan sekitar.\n",
+            "Trenggalek",
+            R.drawable.img_3
+        ),
+        LocationData(
+            "National Park Yosemite",
+            "Goa Lawa merupakan salah satu objek wisata di Trenggalek yang patut anda kunjungi. sejak diresmikannya menjadi objek wisata pada tahun 1984, Gua Lowo atau dalam bahasa jawanya gua kelelawar telah dikunjungi oleh puluhan ribu pengunjung. Sampai sekarang, goa yang terkenal sebagai goa terbesar dan terpanjang di asia tenggara ini masih menyimpan sejuta misteri.",
+            "Trenggalek",
+            R.drawable.img_4
+        ),
+        LocationData(
+            "Pantai Karanggongso",
+            "Pantai ini berjarak 38 Km dari pusat kota Trenggalek. Dengan waktu tempuh sekitar 1 jam perjalanan. Dan hanya berjarak 3 km dari Pantai Prigi yang terkenal dengan pasir putihnya.\n" +
+                    "\n" +
+                    "Destinasi wisata pantai cantik ini menawarkan pemandangan pantai dengan pasir putih yang lembut. Lingkungan pantai yang bersih dengan suasana yang asri. Disepanjang pantai tumbuh rimbun pohon-pohon yang menambah kesejukan udara pantai di siang hari.",
+            "Trenggalek",
+            R.drawable.img_3
+        ),
+        LocationData(
+            "National Park Yosemite",
+            "Pantai Prigi, objek wisata alam di Kabupaten Trenggalek, terletak di Desa Tasikmadu, Kecamatan Watulimo. Selain menjadi bagian dari pusat penjualan ikan terbesar di Jawa Timu. Pantai Prigi juga dilengkapi dengan berbagai fasilitas seperti area parkir, camping site/ground, sport center, hotel, dan restoran. ",
+            "Trenggalek",
+            R.drawable.img_4
+        ),
+        LocationData(
+            "Pantai Pelang",
+            "Hal yang menjadi pembeda dari Pantai Pelang adalah terdapat air terjun yang sangat indah. sangat jarang sekali bisa menemukan pantai yang berdekatan dengan lokasi air terjun. Lokasi air terjun biasa dapat ditemukan di daerah yang termasuk dalam dataran tinggi. Namun hal tersebut berbeda dengan pemandangan yang bisa dilihat di pantai ini Ketika berada di wisata ini, anda akan dapat menemukan air terjun yang lokasinya berada sekitar 300 meter dari pesisir pantai. Lokasi ini biasanya dikunjungi oleh para wisatawan dengan tujuan untuk bermain air ketika ombak di pantai sedang pasang. Untuk bisa sampai ke lokasi air terjun ini, anda perlu berjalan menelusuri pesisir pantai.",
+            "Trenggalek",
+            R.drawable.img_3
+        ),
+        LocationData(
+            "National Park Yosemite",
+            "Keunikan dan keistimewaannya bisa Anda rasakan setelah Anda menapakkan kaki di lokasi setelah berjalan sepanjang 300 meter dari tempat parkir kendaraan. Sesampainya di sana Anda akan diberikan relaksasi alam dari sejuknya udara dan indahnya pemandangan. Air terjun Jurug Gue sendiri memang berada di kawasan perbukitan yang dikelilingi persawahan dan perkebunan penduduk, pepohonan yang rindang, serta perpaduan adanya sungai berkelok dengan bebatuan yang besar.Disamping itu, berbeda dengan air terjun lainnya, yang biasanya air terjun langsung ke bawah, eksotisme air terjun Jurug Gue Trenggalek terlihat dari bagaimana ritme air itu terjun, air terjun ini mengalir melewati tebing bebatuan yang tidak merata dengan ketinggian sekitar 50 meter. Eksotika dari ritme terjunnya air melewati bebatuan ini yang membuat air terjun Jurug Gue begitu memukau untuk dipandang.",
+            "Trenggalek",
+            R.drawable.img_4
+        ),
+        LocationData(
+            "Hutan Kota",
+            "Wisata alam yang terletak ditengah kota yang mampu menarik perhatian warga dan hampir setiap hari ramai dikunjungi. Hutan Kota Trenggalek juga merupakan Hutan Kota yang terbesar di tingkat Kabupaten se-Provinsi Jawa Timur. Kawasan wisata ini menawarkan beragam pesona alam yang asri. Suasananya sangat sejuk dengan pepohonan rindang disekitarnya. Hutan yang terletak di Gunung Jaas, sebelah barat kota Trenggalek. Menyajikan hutan hijau di bukit dengan view kota Trenggalek yang bisa dilihat dari puncak Gunung Jaas.",
+            "Trenggalek",
+            R.drawable.img_3
+        )
+    )
+
+    val HotelDum = arrayListOf(
+        HotelData(
+            "Ubud's Hotel",
+            20,
+            4.9,
+            "Creating a sustainable lifestyle is a meaningful journey and requires the active involvement of every individual.\n" +
+                    "One practical step that can be taken is to understand the impact of every daily action on the environment and look for ways to reduce our ecological footprint.\n" +
+                    "This can start from small decisions such as reducing the use of single-use plastics, choosing eco-friendly products, or recycling waste.\n" +
+                    "The second step that can be taken is to adopt a more thoughtful consumption pattern.\n" +
+                    "This involves choosing products that are durable and recyclable, and avoiding waste. \n" +
+                    "Understanding the life cycle of products, from production to disposal, helps us make more conscious and sustainable choices.\n" +
+                    "In addition, supporting manufacturers and brands that are committed to environmentally friendly production and distribution practices can also have a big impact.\n ",
+            R.drawable.ubud
+        ),
+        HotelData(
+            "Ubud's Hotel",
+            32,
+            4.7,
+            "Creating a sustainable lifestyle is a meaningful journey and requires the active involvement of every individual.\n" +
+                    "One practical step that can be taken is to understand the impact of every daily action on the environment and look for ways to reduce our ecological footprint.\n" +
+                    "This can start from small decisions such as reducing the use of single-use plastics, choosing eco-friendly products, or recycling waste.\n" +
+                    "The second step that can be taken is to adopt a more thoughtful consumption pattern.\n" +
+                    "This involves choosing products that are durable and recyclable, and avoiding waste. \n" +
+                    "Understanding the life cycle of products, from production to disposal, helps us make more conscious and sustainable choices.\n" +
+                    "In addition, supporting manufacturers and brands that are committed to environmentally friendly production and distribution practices can also have a big impact.\n ",
+            R.drawable.img_6
+        ),
+        HotelData(
+            "Ubud's Hotel",
+            40,
+            4.5,
+            "Creating a sustainable lifestyle is a meaningful journey and requires the active involvement of every individual.\n" +
+                    "One practical step that can be taken is to understand the impact of every daily action on the environment and look for ways to reduce our ecological footprint.\n" +
+                    "This can start from small decisions such as reducing the use of single-use plastics, choosing eco-friendly products, or recycling waste.\n" +
+                    "The second step that can be taken is to adopt a more thoughtful consumption pattern.\n" +
+                    "This involves choosing products that are durable and recyclable, and avoiding waste. \n" +
+                    "Understanding the life cycle of products, from production to disposal, helps us make more conscious and sustainable choices.\n" +
+                    "In addition, supporting manufacturers and brands that are committed to environmentally friendly production and distribution practices can also have a big impact.\n ",
+            R.drawable.img_1
+        ),
+        HotelData(
+            "Ubud's Hotel",
+            81,
+            4.4,
+            "Creating a sustainable lifestyle is a meaningful journey and requires the active involvement of every individual.\n" +
+                    "One practical step that can be taken is to understand the impact of every daily action on the environment and look for ways to reduce our ecological footprint.\n" +
+                    "This can start from small decisions such as reducing the use of single-use plastics, choosing eco-friendly products, or recycling waste.\n" +
+                    "The second step that can be taken is to adopt a more thoughtful consumption pattern.\n" +
+                    "This involves choosing products that are durable and recyclable, and avoiding waste. \n" +
+                    "Understanding the life cycle of products, from production to disposal, helps us make more conscious and sustainable choices.\n" +
+                    "In addition, supporting manufacturers and brands that are committed to environmentally friendly production and distribution practices can also have a big impact.\n ",
+            R.drawable.img_6
+        ),
+        HotelData(
+            "Ubud's Hotel",
+            39,
+            4.7,
+            "Creating a sustainable lifestyle is a meaningful journey and requires the active involvement of every individual.\n" +
+                    "One practical step that can be taken is to understand the impact of every daily action on the environment and look for ways to reduce our ecological footprint.\n" +
+                    "This can start from small decisions such as reducing the use of single-use plastics, choosing eco-friendly products, or recycling waste.\n" +
+                    "The second step that can be taken is to adopt a more thoughtful consumption pattern.\n" +
+                    "This involves choosing products that are durable and recyclable, and avoiding waste. \n" +
+                    "Understanding the life cycle of products, from production to disposal, helps us make more conscious and sustainable choices.\n" +
+                    "In addition, supporting manufacturers and brands that are committed to environmentally friendly production and distribution practices can also have a big impact.\n ",
+            R.drawable.img_2
+        ),
+        HotelData(
+            "Ubud's Hotel",
+            59,
+            4.8,
+            "Creating a sustainable lifestyle is a meaningful journey and requires the active involvement of every individual.\n" +
+                    "One practical step that can be taken is to understand the impact of every daily action on the environment and look for ways to reduce our ecological footprint.\n" +
+                    "This can start from small decisions such as reducing the use of single-use plastics, choosing eco-friendly products, or recycling waste.\n" +
+                    "The second step that can be taken is to adopt a more thoughtful consumption pattern.\n" +
+                    "This involves choosing products that are durable and recyclable, and avoiding waste. \n" +
+                    "Understanding the life cycle of products, from production to disposal, helps us make more conscious and sustainable choices.\n" +
+                    "In addition, supporting manufacturers and brands that are committed to environmentally friendly production and distribution practices can also have a big impact.\n ",
+            R.drawable.img_2
+        ),
+        HotelData(
+            "Ubud's Hotel",
+            32,
+            4.7,
+            "Creating a sustainable lifestyle is a meaningful journey and requires the active involvement of every individual.\n" +
+                    "One practical step that can be taken is to understand the impact of every daily action on the environment and look for ways to reduce our ecological footprint.\n" +
+                    "This can start from small decisions such as reducing the use of single-use plastics, choosing eco-friendly products, or recycling waste.\n" +
+                    "The second step that can be taken is to adopt a more thoughtful consumption pattern.\n" +
+                    "This involves choosing products that are durable and recyclable, and avoiding waste. \n" +
+                    "Understanding the life cycle of products, from production to disposal, helps us make more conscious and sustainable choices.\n" +
+                    "In addition, supporting manufacturers and brands that are committed to environmentally friendly production and distribution practices can also have a big impact.\n ",
+            R.drawable.img_6
+        ),
+        HotelData(
+            "Ubud's Hotel",
+            32,
+            4.7,
+            "Creating a sustainable lifestyle is a meaningful journey and requires the active involvement of every individual.\n" +
+                    "One practical step that can be taken is to understand the impact of every daily action on the environment and look for ways to reduce our ecological footprint.\n" +
+                    "This can start from small decisions such as reducing the use of single-use plastics, choosing eco-friendly products, or recycling waste.\n" +
+                    "The second step that can be taken is to adopt a more thoughtful consumption pattern.\n" +
+                    "This involves choosing products that are durable and recyclable, and avoiding waste. \n" +
+                    "Understanding the life cycle of products, from production to disposal, helps us make more conscious and sustainable choices.\n" +
+                    "In addition, supporting manufacturers and brands that are committed to environmentally friendly production and distribution practices can also have a big impact.\n ",
+            R.drawable.img_6
+        )
+    )
+}
