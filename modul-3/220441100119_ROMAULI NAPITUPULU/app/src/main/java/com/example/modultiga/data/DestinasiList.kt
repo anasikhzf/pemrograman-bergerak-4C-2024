10ccba8d30fd706bca793da1f199f26138172098
romaulinapitupulu <napitupuluromauli044@gmail.com>
2024-05-15 21:08:27 +0700

    220441100119-ROMAULI NAPITUPULU-MOHAMMAD IQBAL FIRMANSYAH-MODUL 3

---
+++ modul-3/220441100119_ROMAULI NAPITUPULU/app/src/main/java/com/example/modultiga/data/DestinasiList.kt
@@ -0,0 +1,131 @@
+package com.example.modultiga.data
+
+import com.example.modultiga.R
+
+object DestinasiList {
+    val LokasiDum = arrayListOf(
+        LocationData(
+            "Pantai Kuyon",
+            "Desa Wisata Bahari Nglebeng di Kabupaten Trenggalek mempunyai produk wisata Pantai Kuyon. Pantai Kuyon memiliki keindahan pantai yang berbatasan dengan sawah yang menambah kecantikan pantai. Pantai Kuyon dapat dijadikan lokasi liburan di akhir pekan bersama teman-teman untuk refreshing.\n",
+            "Trenggalek",
+            R.drawable.kuyon
+        ),
+        LocationData(
+            "Pantai Kebo",
+            "Pantai Kebo Trenggalek memiliki spot yang cukup istimewa berupa rerumputan hijau luas dengan aksen aliran air bagaikan sungai dan beberapa ekor kerbau. Latar belakang pantai berupa bukit-bukit hijau yang mampu menambah alami pemandangan sekitar.\n",
+            "Trenggalek",
+            R.drawable.kebo
+        ),
+        LocationData(
+            "Goa Lawa",
+            "Goa Lawa merupakan salah satu objek wisata di Trenggalek yang patut anda kunjungi. sejak diresmikannya menjadi objek wisata pada tahun 1984, Gua Lowo atau dalam bahasa jawanya gua kelelawar telah dikunjungi oleh puluhan ribu pengunjung. Sampai sekarang, goa yang terkenal sebagai goa terbesar dan terpanjang di asia tenggara ini masih menyimpan sejuta misteri.",
+            "Trenggalek",
+            R.drawable.goalawa
+        ),
+        LocationData(
+            "Pantai Karanggongso",
+            "Pantai ini berjarak 38 Km dari pusat kota Trenggalek. Dengan waktu tempuh sekitar 1 jam perjalanan. Dan hanya berjarak 3 km dari Pantai Prigi yang terkenal dengan pasir putihnya.\n" +
+                    "\n" +
+                    "Destinasi wisata pantai cantik ini menawarkan pemandangan pantai dengan pasir putih yang lembut. Lingkungan pantai yang bersih dengan suasana yang asri. Disepanjang pantai tumbuh rimbun pohon-pohon yang menambah kesejukan udara pantai di siang hari.",
+            "Trenggalek",
+            R.drawable.karanggongso
+        ),
+        LocationData(
+            "Pantai Prigi",
+            "Pantai Prigi, objek wisata alam di Kabupaten Trenggalek, terletak di Desa Tasikmadu, Kecamatan Watulimo. Selain menjadi bagian dari pusat penjualan ikan terbesar di Jawa Timu. Pantai Prigi juga dilengkapi dengan berbagai fasilitas seperti area parkir, camping site/ground, sport center, hotel, dan restoran. ",
+            "Trenggalek",
+            R.drawable.prigi
+        ),
+        LocationData(
+            "Pantai Pelang",
+            "Hal yang menjadi pembeda dari Pantai Pelang adalah terdapat air terjun yang sangat indah. sangat jarang sekali bisa menemukan pantai yang berdekatan dengan lokasi air terjun. Lokasi air terjun biasa dapat ditemukan di daerah yang termasuk dalam dataran tinggi. Namun hal tersebut berbeda dengan pemandangan yang bisa dilihat di pantai ini Ketika berada di wisata ini, anda akan dapat menemukan air terjun yang lokasinya berada sekitar 300 meter dari pesisir pantai. Lokasi ini biasanya dikunjungi oleh para wisatawan dengan tujuan untuk bermain air ketika ombak di pantai sedang pasang. Untuk bisa sampai ke lokasi air terjun ini, anda perlu berjalan menelusuri pesisir pantai.",
+            "Trenggalek",
+            R.drawable.pelang
+        ),
+        LocationData(
+            "Jurug Gue",
+            "Keunikan dan keistimewaannya bisa Anda rasakan setelah Anda menapakkan kaki di lokasi setelah berjalan sepanjang 300 meter dari tempat parkir kendaraan. Sesampainya di sana Anda akan diberikan relaksasi alam dari sejuknya udara dan indahnya pemandangan. Air terjun Jurug Gue sendiri memang berada di kawasan perbukitan yang dikelilingi persawahan dan perkebunan penduduk, pepohonan yang rindang, serta perpaduan adanya sungai berkelok dengan bebatuan yang besar.Disamping itu, berbeda dengan air terjun lainnya, yang biasanya air terjun langsung ke bawah, eksotisme air terjun Jurug Gue Trenggalek terlihat dari bagaimana ritme air itu terjun, air terjun ini mengalir melewati tebing bebatuan yang tidak merata dengan ketinggian sekitar 50 meter. Eksotika dari ritme terjunnya air melewati bebatuan ini yang membuat air terjun Jurug Gue begitu memukau untuk dipandang.",
+            "Trenggalek",
+            R.drawable.jurug
+        ),
+        LocationData(
+            "Hutan Kota",
+            "Wisata alam yang terletak ditengah kota yang mampu menarik perhatian warga dan hampir setiap hari ramai dikunjungi. Hutan Kota Trenggalek juga merupakan Hutan Kota yang terbesar di tingkat Kabupaten se-Provinsi Jawa Timur. Kawasan wisata ini menawarkan beragam pesona alam yang asri. Suasananya sangat sejuk dengan pepohonan rindang disekitarnya. Hutan yang terletak di Gunung Jaas, sebelah barat kota Trenggalek. Menyajikan hutan hijau di bukit dengan view kota Trenggalek yang bisa dilihat dari puncak Gunung Jaas.",
+            "Trenggalek",
+            R.drawable.huko
+        )
+    )
+
+    val HotelDum = arrayListOf(
+        HotelData(
+            "Hayam Wuruk",
+            20,
+            4.9,
+            "Hayam Wuruk Hotel and Convention menawarkan akomodasi di Trenggalek dan berjarak sejauh 1 km dari Stadion Menak Sopal Trenggalek. Properti ini memiliki Wi-Fi gratis.\n" +
+                    "Setiap kamar dilengkapi dengan AC. Shower tersedia di kamar mandi.\n" +
+                    "Tamu dapat menikmati makanan di Lamongan Coto by Cak Ji. Pilihan tempat makan lain juga tersedia di sekitar properti.\n" +
+                    "Fasilitas lain yang tersedia di Hayam Wuruk Hotel and Convention adalah ruang pertemuan.\n" +
+                    "Stasiun terdekat adalah Stasiun Kediri, 58.8 km dari properti. ",
+            R.drawable.hayamwuruk
+        ),
+        HotelData(
+            "Jaas Permai",
+            32,
+            4.7,
+            "Bukit Jaas Permai Hotel adalah tempat bermalam yang tepat bagi Anda yang berlibur bersama keluarga. Nikmati segala fasilitas hiburan untuk Anda dan keluarga.\n" +
+                    "\n" +
+                    "Pelayanan memuaskan serta fasilitas hotel yang memadai akan membuat Anda nyaman berada di Bukit Jaas Permai Hotel.\n" +
+                    "\n" +
+                    "Resepsionis siap 24 jam untuk melayani proses check-in, check-out dan kebutuhan Anda yang lain. Jangan ragu untuk menghubungi resepsionis, kami siap melayani Anda.\n" +
+                    "\n" +
+                    "WiFi tersedia di seluruh area publik properti untuk membantu Anda tetap terhubung dengan keluarga dan teman.\n" +
+                    "\n" +
+                    "Bukit Jaas Permai Hotel adalah akomodasi dengan fasilitas baik dan kualitas pelayanan memuaskan menurut sebagian besar tamu.\n" +
+                    "\n" +
+                    "Dengan fasilitas yang memadai, Bukit Jaas Permai Hotel menjadi pilihan yang tepat untuk menginap. ",
+            R.drawable.jaas
+        ),
+        HotelData(
+            "Griya Wafira",
+            40,
+            4.5,
+            "Terletak di Trenggalek, OYO 93525 Griya Wafira Trenggalek Syariah menawarkan akomodasi bintang 2 dengan balkon pribadi. Di hotel, semua kamar dilengkapi AC dan TV. Bandara terdekat adalah Bandara Abdul Rachman Saleh, 151 km dari OYO 93525 Griya Wafira Trenggalek Syariah. ",
+            R.drawable.griyawafira
+        ),
+        HotelData(
+            "Griya Anggrek",
+            81,
+            4.4,
+            "Griya Anggrek homestay yang terletak di Jl. Moh Yamin Sawahan Sumber Gedong trenggalek, dengan total 21 kamar, memiliki beberapa fasilitas, antara lain - Parkir area - Waterheater - Wifi - Televisi - Laundry service dan masih banyak lainnya. Dekat dengan tempat wisata trenggalek, dan cocok untuk kebutuhan bisnis atau berlibur anda. ",
+            R.drawable.griyaanggrek
+        ),
+        HotelData(
+            "OYO 2966 Hotel Atriaz",
+            39,
+            4.7,
+            "OYO 2966 Hotel Atriaz menawarkan kamar dengan tempat tidur yang nyaman untuk waktu menginap yang menyenangkan. Kamar dilengkapi dengan tempat tidur yang nyaman, wifi, AC, Televisi, kamar mandi yang higienis, dan perelengkapan dengan kualitas tinggi dan masa kini.",
+            R.drawable.atriaz
+        ),
+        HotelData(
+            "Karanggoso Indah Hotel",
+            59,
+            4.8,
+            "Karanggoso Indah Hotel menawarkan akomodasi di Trenggalek dan berjarak sejauh 280 meter dari Pantai Karanggongso. Properti ini memiliki akses WiFi gratis. Setiap kamar dilengkapi dengan tempat tidur yang nyaman & AC. Shower tersedia di kamar mandi. Tamu dapat menikmati makanan di Della Seafood. Pilihan tempat makan lain juga tersedia di sekitar properti. Fasilitas lain yang tersedia di Karanggoso Indah Hotel adalah TV layar datar. Bandara terdekat adalah Bandar Udara Abdul Rachman Saleh, 161 km dari properti. ",
+            R.drawable.karanggongsohotel
+        ),
+        HotelData(
+            "Jaas Permai",
+            32,
+            4.7,
+            "Hot ",
+            R.drawable.hayamwuruk
+        ),
+        HotelData(
+            "Jaas Permai",
+            32,
+            4.7,
+            "Hot ",
+            R.drawable.hayamwuruk
+        )
+    )
+}
