d8522b1eae22ad21daef2d7fdf5b4f6e8298cbf5
adeliashafiraa <113223407+adeliashafiraa@users.noreply.github.com>
2024-05-15 17:39:47 +0700

    first commit

--- modul-3/220441100104_AdeliaShafiraArdhani/modul3/modul3/app/src/main/java/com/example/modultiga/data/HotelData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.modultiga.data
-
-data class HotelData(
-    val nama : String,
-    val jumlah_pengunjung : Int,
-    val rating : Double,
-    val deskripsi : String,
-    val gambar : Int
-)
--- modul-3/220441100104_AdeliaShafiraArdhani/modul3/modul3/app/src/main/java/com/example/modultiga/data/LocationData.kt
+++
@@ -1,8 +0,0 @@
-package com.example.modultiga.data
-
-data class LocationData(
-    val name: String,
-    val description: String,
-    val lokasi: String,
-    val image: Int
-)
