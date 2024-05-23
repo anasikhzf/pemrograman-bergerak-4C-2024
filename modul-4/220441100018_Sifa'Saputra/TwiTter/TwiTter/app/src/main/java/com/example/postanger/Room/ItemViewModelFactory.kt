8e0948e9c864c37bb798c5006a8e96ab0904f67b
sipdohc <sifasaputramb17@gmail.com>
2024-05-23 11:48:03 +0700

    220441100018-SIFA' SAPUTRA-MOCHAMMAD SYAHRUL ABIDIN-MODUL 4

---
+++ modul-4/220441100018_Sifa'Saputra/TwiTter/TwiTter/app/src/main/java/com/example/postanger/Room/ItemViewModelFactory.kt
@@ -0,0 +1,27 @@
+package com.example.twitter.Room
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.twitter.Utils.DependencyInjection
+
+
+class ItemViewModelFactory private constructor(private val itemRepository: ItemRepository) :
+    ViewModelProvider.NewInstanceFactory() {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ItemViewModel::class.java)) {
+            return ItemViewModel(itemRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
+
+    companion object {
+        @Volatile
+        private var instance: ItemViewModelFactory? = null
+        fun getInstance(context: Context): ItemViewModelFactory =
+            instance ?: synchronized(this) {
+                instance ?: ItemViewModelFactory(DependencyInjection.provideRepository(context))
+            }.also { instance = it }
+    }
+}
