fb9ff5486a153dc25faa992876f73473536088d8
Husnulkhotima <husnulkhotima54111@gmail.com>
2024-06-02 09:21:47 +0700

    220441100038-HUSNULKHOTIMA-MOCHAMMAD SYAHRUL ABIDIN-MODUL5

---
+++ modul-5/220441100038-HusnulKhotima/Modul4zip/modul4/app/src/main/java/com/example/modulempat/update_post.kt
@@ -0,0 +1,20 @@
+package com.example.modulempat
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class update_post : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_update_post)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.post_img_update)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
